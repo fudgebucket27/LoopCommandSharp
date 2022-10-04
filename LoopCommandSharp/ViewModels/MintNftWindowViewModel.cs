6bf6baebb0ec26780e5f682159092be690c77e28
Fudgey <fudgebucket27@gmail.com>
2022-10-04 16:31:22 +1000

    mint nft ui boilerplate

--- LoopCommandSharp/ViewModels/MintNftWindowViewModel.cs
+++ LoopCommandSharp/ViewModels/MintNftWindowViewModel.cs
@@ -1,19 +1,66 @@
 ﻿using LoopCommandSharp.Models;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LoopCommandSharp.ViewModels 
 {
-    public class MintNftWindowViewModel : ViewModelBase
+    public class MintNftWindowViewModel : ViewModelBase, INotifyPropertyChanged
     {
         public Settings Settings { get; set; }
+
+        public bool IsMinting { get; set; } = false;
+
+        public string Cids { get; set; }
+
+        public string log { get; set; }
+
+        public string Log
+        {
+            get => log;
+            set
+            {
+                log = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Log)));
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ReactiveCommand<Unit, Unit> MintNft { get; }
 
         public MintNftWindowViewModel(Settings settings)
         {
             Settings = settings;
+            MintNft = ReactiveCommand.Create(Mint);
+        }
+
+        void Mint()
+        {
+            using (StringReader reader = new StringReader(Cids))
+            {
+                string line = string.Empty;
+                int count = 1;
+                do
+                {
+                    line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        Log += $"Minted {count}" + Environment.NewLine;
+                        count++;
+                    }
+                   
+
+                } while (line != null);
+            }
         }
     }
 }
